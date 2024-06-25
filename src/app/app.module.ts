23e87986a8e4cc5ceb63c6b512786faf620a88f8
Cristobal Rosa <crisrogar@gmail.com>
2024-06-25 13:49:23 +0200

    create autocomplete component basic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { InputComponent } from './input/input.component';
 import { MaterialModule } from '../Material.Module';
+import { AutocompleteComponent } from './component/autocomplete/autocomplete.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InputComponent
+    InputComponent,
+    AutocompleteComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component/autocomplete/autocomplete.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-autocomplete',
+  templateUrl: './autocomplete.component.html',
+  styleUrl: './autocomplete.component.css'
+})
+export class AutocompleteComponent {
+
+  colorArray: string[] = ['Red', 'Green', 'Value'];
+
+}
