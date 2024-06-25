f39f0728af399baa4fcddf61cebb92af5bb9216b
Cristobal Rosa <crisrogar@gmail.com>
2024-06-25 13:03:56 +0200

    create select basic

--- src/Material.Module.ts
+++ src/Material.Module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from "@angular/core";
 import { MatInputModule } from "@angular/material/input";
+import { MatSelectModule } from "@angular/material/select";
 
 @NgModule({
   exports: [
-    MatInputModule
+    MatInputModule,
+    MatSelectModule
   ]
 })
 export class MaterialModule{}
--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+interface Food {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
   styleUrl: './input.component.css'
 })
 export class InputComponent {
+
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'},
+  ]
 
 }
