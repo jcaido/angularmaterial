9b5bdaca0aac98acf6a91517a090355c18ceb2f2
Cristobal Rosa <crisrogar@gmail.com>
2024-06-27 12:31:19 +0200

    include datepicker-in-line into barmenu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { AutocompleteComponent } from './component/autocomplete/autocomplete.component';
 import { InputComponent } from './input/input.component';
 import { HomeComponent } from './home/home.component';
+import { DatepickerInLineComponent } from './component/datepicker-in-line/datepicker-in-line.component';
 
 const routes: Routes = [
   {path:'home', component: HomeComponent},
+  {path:'datepicker', component: DatepickerInLineComponent},
   {path:'autocomplete', component: AutocompleteComponent},
   {path:'input', component: InputComponent}
 ];
