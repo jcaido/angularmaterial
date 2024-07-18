3a2c5bd9710a2547cc4077e1cf206a0c4241d9f3
Cristobal Rosa <crisrogar@gmail.com>
2024-07-18 20:45:08 +0200

    update app-routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { CalendarComponent } from './component/calendar/calendar.component';
 import { CardComponent } from './component/card/card.component';
 import { SliderComponent } from './component/slider/slider.component';
 import { TableComponent } from './component/table/table.component';
+import { FormdesignComponent } from './component/formdesign/formdesign.component';
 
 const routes: Routes = [
   {path:'home', component: HomeComponent},
@@ -16,7 +17,8 @@ const routes: Routes = [
   {path:'input', component: InputComponent},
   {path:'card', component: CardComponent},
   {path:'slider', component: SliderComponent},
-  {path:'table', component: TableComponent}
+  {path:'table', component: TableComponent},
+  {path:'formdesign', component: FormdesignComponent}
 ];
 
 @NgModule({
