a7f9703348c967057b3994d6b7e3415b2830543d
Cristobal Rosa <crisrogar@gmail.com>
2024-07-20 21:51:22 +0200

    implement paginator en table component

--- src/app/component/table/table.component.ts
+++ src/app/component/table/table.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { MasterService } from '../../service/master.service';
 import { Customer } from '../../Model/Customer';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-table',
@@ -14,12 +15,98 @@ export class TableComponent {
   dataSource: any;
   displayedColumns: string[] = ["code", "name", "email", "phone", "status", "action"];
 
-  constructor(private service: MasterService) {
-    service.GetCustomer().subscribe(
-      res => {
-        this.customerList = res;
-        this.dataSource = new MatTableDataSource<Customer>(this.customerList);
-      })
+
+  listCustomer :Customer[] = [
+    {
+      code: "1",
+      name: "Janesh",
+      email: "janesh@in.com",
+      phone: "a989787867",
+      active: "Active"
+    },
+    {
+      code: "2",
+      name: "Bishoi",
+      email: "bishoi@in.com",
+      phone: "8989787867",
+      active: "In Active"
+    },
+    {
+      code: "3",
+      name: "surya",
+      email: "surya@in.com",
+      phone: "8989787867",
+      active: "In Active"
+    },
+    {
+      code: "4",
+      name: "kamlesh",
+      email: "kamlesh@in.com",
+      phone: "8989787867",
+      active: "In Active"
+    },
+    {
+      code: "5",
+      name: "Kriti shetty",
+      email: "kriti@in.com",
+      phone: "8989787867",
+      active: "Active"
+    },
+    {
+      code: "6",
+      name: "Tammanah",
+      email: "tam@in.com",
+      phone: "8989787867",
+      active: ""
+    },
+    {
+      code: "7",
+      name: "Priyanka Mohan",
+      email: "priya@in.com",
+      phone: "8989787867",
+      active: "Active"
+    },
+    {
+      code: "8",
+      name: "rashmika",
+      email: "rashmika@in.com",
+      phone: "8989787867",
+      active: "Active"
+    },
+    {
+      code: "9",
+      name: "Trisha",
+      email: "trisha@in.com",
+      phone: "8989787867",
+      active: "Active"
+    },
+    {
+      code: "10",
+      name: "samanta",
+      email: "samantha@in.com",
+      phone: "8989787867",
+      active: "Active"
+    }
+  ]
+
+  data: any = new MatTableDataSource<Customer>(this.listCustomer);
+  @ViewChild(MatPaginator) paginator! :MatPaginator;
+  //data: any = new MatTableDataSource<Customer>(this.listCustomer);
+  //this.data.paginatior = this.paginatior;
+
+
+  //constructor(private service :MasterService) {
+  //  service.GetCustomer().subscribe(
+  //    res => {
+  //      this.customerList = res;
+  //      this.dataSource = new MatTableDataSource<Customer>(this.customerList);
+  //    })
+  //}
+
+  constructor(){}
+
+  ngAfterViewInit() {
+    this.data.paginator = this.paginator;
   }
 
 }
